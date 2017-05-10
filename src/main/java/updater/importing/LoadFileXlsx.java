9c286796395eee8d6709767eb9af07e623269d17
mariusz <mariusz@172.16.17.106>
2017-05-10 11:26:29 +0200

    odczyt excela
    zmienione zaleznosci w pom.xml

--- src/main/java/updater/importing/LoadFileXlsx.java
+++ src/main/java/updater/importing/LoadFileXlsx.java
@@ -15,12 +15,19 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class LoadFileXlsx extends Load {
 	private FileInputStream fis;
 	private XSSFWorkbook workBook;
+	/**
+	 * aktualnie wykorzystywany arkusz
+	 */
 	private XSSFSheet sheet;
 	private Iterator<Row> rowIterator;
+	/**
+	 * maksymalna liczba wierszy w wybranym arkuszu
+	 */
 	private int numberOfRow;
+	/**
+	 * maksymalna liczba kolumn w wybranym arkuszu
+	 */
 	private int numberOfCell;
-	
-	
 
 	public int getNumberOfRow() {
 		return numberOfRow;
@@ -38,13 +45,25 @@ public class LoadFileXlsx extends Load {
 		this.numberOfCell = numberOfCell;
 	}
 
+	/**
+	 * Konstruktor, ktory od razu startuje z metodami niezbednymi do obslugi
+	 * klasy
+	 */
 	public LoadFileXlsx() {
 		super();
 		selectFile();
 	}
 
+	/**
+	 * Metoda odpowiedzialna za wybor pliku excela do importu
+	 * 
+	 * ROZWOJ
+	 * 
+	 * Powinna byc mozliwosc importowania wielu plikow xlsx i kolejkowania
+	 * operacji na tych plikach
+	 */
 	public void selectFile() {
-		file = new File("D://hbi//test2.xlsx");
+		file = new File("D://hbi//test.xlsx");
 		if (file.exists()) {
 			logger.info("File selected - " + file.getPath());
 			loadData();
@@ -52,29 +71,33 @@ public class LoadFileXlsx extends Load {
 			logger.warn("The file not exist!");
 	}
 
+	/**
+	 * Metoda odczytujaca plik xlsx, ktorego adres zdefiniowany jest w obiekcie
+	 * File file
+	 */
 	public void loadData() {
 		try {
 			fis = new FileInputStream(file);
 			logger.debug("fis object created");
-			try{
+			try {
 				logger.debug("workBook object before create");
 				workBook = new XSSFWorkbook(fis);
 				logger.debug("workBook object after created");
-			}catch(Exception e){
+			} catch (Exception e) {
 				logger.error(e.getMessage(), e);
 				e.printStackTrace();
 			}
 			logger.debug("workbook object after created");
 			// interesuje nas zawsze pierwszy arkusz
-			try{
+			try {
 				logger.debug("sheet object before created");
 				sheet = workBook.getSheetAt(0);
 				logger.debug("sheet object after created");
-			}catch(Exception e){
-				logger.error("unable to create sheet: " +e.getMessage());
+			} catch (Exception e) {
+				logger.error("unable to create sheet: " + e.getMessage());
 				e.printStackTrace();
 			}
-			
+
 			rowIterator = sheet.iterator();
 			// iteracja wierszy
 			while (rowIterator.hasNext()) {
@@ -84,12 +107,12 @@ public class LoadFileXlsx extends Load {
 				Iterator<Cell> cellIterator = row.cellIterator();
 
 				// iteracja kolumn
-				CellValue  value = new CellValue(null);
+				CellValue value = new CellValue(null);
 				while (cellIterator.hasNext()) {
 					// biezaca komorka
 					Cell cell = cellIterator.next();
 					value = returnCell(cell);
-					debugLog.info(value.getStringValue()+";");
+					debugLog.info(value.getStringValue() + ";");
 				}
 			}
 		} catch (FileNotFoundException e) {
@@ -101,6 +124,14 @@ public class LoadFileXlsx extends Load {
 		}
 	}
 
+	/**
+	 * Zwracanie wartosci poszczegolnych komorek w zaleznosci od typu danych
+	 * komorki FORMULA nie jest zaimplementowana, poniewaz nie przewiduje sie
+	 * tego typu danych
+	 * 
+	 * @param cell
+	 * @return
+	 */
 	public CellValue returnCell(Cell cell) {
 		if (cell == null)
 			return null;
@@ -122,18 +153,22 @@ public class LoadFileXlsx extends Load {
 			throw new IllegalStateException("Bad cell type (" + cell.getCellTypeEnum() + ")");
 		}
 	}
-	
-	public void setRange(XSSFSheet sheet){
+
+	/**
+	 * Metoda ustala maksymalny rozmiar arkusza (wiersze oraz kolumny)
+	 * 
+	 * @param sheet
+	 */
+	public void setRange(XSSFSheet sheet) {
 		this.setNumberOfRow(sheet.getLastRowNum());
 		this.setNumberOfCell(0);
 		Iterator<Row> findMaxCellIterator = sheet.iterator();
-		while(findMaxCellIterator.hasNext()){
+		while (findMaxCellIterator.hasNext()) {
 			Row row = findMaxCellIterator.next();
-			if(row.getLastCellNum()>this.getNumberOfCell())this.setNumberOfCell(row.getLastCellNum());
+			if (row.getLastCellNum() > this.getNumberOfCell())
+				this.setNumberOfCell(row.getLastCellNum());
 		}
-		logger.info("Sheet range: rows:"+this.getNumberOfRow()+", cells:"+this.getNumberOfCell());
+		logger.info("Sheet range: rows:" + this.getNumberOfRow() + ", cells:" + this.getNumberOfCell());
 	}
-	
-
 
 }
--- src/main/java/updater/run/StartTest.java
+++ src/main/java/updater/run/StartTest.java
@@ -1,19 +1,23 @@
 package updater.run;
 
+//import org.apache.log4j.Logger;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import updater.importing.LoadData;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
-import org.apache.log4j.Logger;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.util.Iterator;
 
 public class StartTest {
 
 //	public static Logger logger = Logger.getLogger(StartTest.class);
 	public static void main(String[] args) {
 //		logger.info("updater start");
+		System.out.println("Start");
 		start();
 	}
 	static void start(){
@@ -21,25 +25,33 @@ public class StartTest {
 //		logger.info("entityManagerFactory - stworzono");
 		
 //		entityManagerFactory.close();
-//		LoadData loadData = new LoadData();
-		 File excel =  new File ("D:/hbi/test2.xlsx");
-	        FileInputStream fis;
-			try {
-				fis = new FileInputStream(excel);
-				 XSSFWorkbook wb;
-					try {
-						wb = new XSSFWorkbook(fis);
-//						XSSFSheet ws = wb.getSheet("Input");
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-//					logger.info("ZROBIONE");
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	       
+		LoadData loadData = new LoadData();
+//		 File excel =  new File ("D:/hbi/test2.xlsx");
+//		 try {
+//	            FileInputStream excelFile = new FileInputStream(new File("D://hbi//test.xlsx"));
+//	            Workbook workbook = new XSSFWorkbook(excelFile);
+//	            Sheet datatypeSheet = workbook.getSheetAt(0);
+//	            Iterator<Row> iterator = datatypeSheet.iterator();
+//	            while (iterator.hasNext()) {
+//	                Row currentRow = iterator.next();
+//	                Iterator<Cell> cellIterator = currentRow.iterator();
+//	                while (cellIterator.hasNext()) {
+//	                    Cell currentCell = cellIterator.next();
+//	                    //getCellTypeEnum shown as deprecated for version 3.15
+//	                    //getCellTypeEnum ill be renamed to getCellType starting from version 4.0
+//	                    if (currentCell.getCellTypeEnum() == CellType.STRING) {
+//	                    	 System.out.println(currentCell.getStringCellValue() + "--");
+//	                    } else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
+//	                    	 System.out.println(currentCell.getNumericCellValue() + "--");
+//	                    }
+//	                }
+//	                System.out.println("nowa linia");
+//	            }
+//	            workbook.close();
+//	        } catch (FileNotFoundException e) {
+//	            e.printStackTrace();
+//	        } catch (IOException e) {
+//	            e.printStackTrace();
+//	        }	       
 	}
-
 }
